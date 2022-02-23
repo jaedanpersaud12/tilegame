2912f48bc30d89f4670222ad7564e4f4822c2664
Jaedan Persaud <63131031+jaedanpersaud12@users.noreply.github.com>
2022-02-23 10:51:39 -0400

    lol

---
+++ script.js
@@ -0,0 +1,9 @@
+var pairs = [[1,11],[2,7],[3.15],[4,5],[6,12],[8,10],[9,14],[13,16]]
+
+var currentPair = [1,11]
+
+if(currentPair == pairs[0]){
+    console.log("true");
+}else{
+    console.log("false");
+}
